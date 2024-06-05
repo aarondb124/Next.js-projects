2717f0329b24c9d9df7ea71177f62e5dfeceda5c
Aaron Debakor BIswas <aarondb124@gmail.com>
2024-06-05 19:39:25 +0600

    My first commit

---
+++ first-next-app/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h1>Hello about. How are you?</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ first-next-app/pages/blogpost/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      How to contact me?
+    </div>
+  )
+}
+
+export default contact
